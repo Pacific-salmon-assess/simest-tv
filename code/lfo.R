409fdbe80d5c7b5bfa6b292752603c72f0ae92f8
catarinawor <catarinawor@gmail.com>
2022-10-26 17:05:58 -0700

    add model weights to TMB lfo

--- code/lfo.R
+++ code/lfo.R
@@ -37,6 +37,7 @@ dirNames <- sapply(scenNames, function(x) paste(x, unique(simPar$species),sep = 
 plotscn <- TRUE
 p <- list()
 lfoTMB <- list()
+lfomwTMB <- list()
 
 for(a in seq_len(nrow(simPar))){
    #a=2
@@ -44,11 +45,12 @@ for(a in seq_len(nrow(simPar))){
   simData <- readRDS(paste0("outs/SamSimOutputs/simData/", simPar$nameOM[a],"/",simPar$scenario[a],"/",
                          paste(simPar$nameOM[a],"_", simPar$nameMP[a], "_", "CUsrDat.RData",sep="")))$srDatout
 
-  lfodf<-matrix(NA,nrow=length(unique(simData$iteration)),ncol=26,
+  lfodf<-matrix(NA,nrow=length(unique(simData$iteration)),ncol=29,
   dimnames = list(unique(simData$iteration),
     c("simple", "autocorr", 
       "rwa_last","rwa_last3","rwa_last5",
       "rwb_last","rwb_last3","rwb_last5",
+      "rwab_last","rwab_last3","rwab_last5",
       "hmma_last_pick","hmma_last3_pick","hmma_last5_pick",
       "hmma_last_average","hmma_last3_average","hmma_last3_average",
       "hmmb_last_pick","hmmb_last3_pick","hmmb_last5_pick",
@@ -56,6 +58,20 @@ for(a in seq_len(nrow(simPar))){
       "hmm_last_pick", "hmm_last3_pick", "hmm_last5_pick",
       "hmm_last_average","hmm_last3_average","hmm_last5_average"
       )))
+  lfomwdf<-matrix(NA,nrow=length(unique(simData$iteration)),ncol=29,
+  dimnames = list(unique(simData$iteration),
+    c("simple", "autocorr", 
+      "rwa_last","rwa_last3","rwa_last5",
+      "rwb_last","rwb_last3","rwb_last5",
+      "rwab_last","rwab_last3","rwab_last5",
+      "hmma_last_pick","hmma_last3_pick","hmma_last5_pick",
+      "hmma_last_average","hmma_last3_average","hmma_last3_average",
+      "hmmb_last_pick","hmmb_last3_pick","hmmb_last5_pick",
+      "hmmb_last_average","hmmb_last3_average","hmmb_last5_average",
+      "hmm_last_pick", "hmm_last3_pick", "hmm_last5_pick",
+      "hmm_last_average","hmm_last3_average","hmm_last5_average"
+      )))
+ 
  
   for(u in unique(simData$iteration)){
     dat<-simData[simData$iteration==u,]
@@ -77,7 +93,37 @@ for(a in seq_len(nrow(simPar))){
     lfohmma <- tmb_mod_lfo_cv(data=df,model='HMM_a', L=round((2/3)*nrow(dat)))
     lfohmmb <- tmb_mod_lfo_cv(data=df,model='HMM_b', L=round((2/3)*nrow(dat)))
     lfohmm <- tmb_mod_lfo_cv(data=df,model='HMM', L=round((2/3)*nrow(dat)))
+    
 
+    LLdf<-rbind(lfostatic$lastparam,lfoac$lastparam,
+      lfoalpha$lastparam,lfoalpha$last3param,lfoalpha$last5param,
+      lfobeta$lastparam,lfobeta$last3param,lfobeta$last5param,
+      lfoalphabeta$lastparam,lfoalphabeta$last3param,lfoalphabeta$last5param,
+      lfohmma$lastregime_pick,lfohmma$last3regime_pick,lfohmma$last5regime_pick,
+      lfohmma$lastregime_average,lfohmma$last3regime_average,lfohmma$last5regime_average,
+      lfohmmb$lastregime_pick,lfohmmb$last3regime_pick,lfohmmb$last5regime_pick,
+      lfohmmb$lastregime_average,lfohmmb$last3regime_average,lfohmmb$last5regime_average,
+      lfohmm$lastregime_pick,lfohmm$last3regime_pick,lfohmm$last5regime_pick,
+      lfohmm$lastregime_average,lfohmm$last3regime_average,lfohmm$last5regime_average
+      )
+    rownames(LLdf)<-colnames(lfodf)
+    
+    
+    convdf<-rbind(lfostatic$conv_problem,lfoac$conv_problem,
+      lfoalpha$conv_problem,lfoalpha$conv_problem,lfoalpha$conv_problem,
+      lfobeta$conv_problem,lfobeta$conv_problem,lfobeta$conv_problem,
+      lfoalphabeta$conv_problem,lfoalphabeta$conv_problem,lfoalphabeta$conv_problem,
+      lfohmma$conv_problem,lfohmma$conv_problem,lfohmma$conv_problem,
+      lfohmma$conv_problem,lfohmma$conv_problem,lfohmma$conv_problem,
+      lfohmmb$conv_problem,lfohmmb$conv_problem,lfohmmb$conv_problem,
+      lfohmmb$conv_problem,lfohmmb$conv_problem,lfohmmb$conv_problem,
+      lfohmm$conv_problem,lfohmm$conv_problem,lfohmm$conv_problem,
+      lfohmm$conv_problem,lfohmm$conv_problem,lfohmm$conv_problem
+      )
+
+    mw<-model_weights(LLdf)
+    mw[as.logical(apply(convdf,1,sum))]<-0
+    lfomwdf[u,] <- mw
     lfodf[u,] <- c(ifelse(sum(lfostatic$conv_problem)>0,0,sum(lfostatic$lastparam)), 
       ifelse(sum(lfoac$conv_problem)>0,999,sum(lfoac$lastparam)), 
       ifelse(sum(lfoalpha$conv_problem)>0,999,sum(lfoalpha$lastparam)), 
@@ -85,7 +131,10 @@ for(a in seq_len(nrow(simPar))){
       ifelse(sum(lfoalpha$conv_problem)>0,999,sum(lfoalpha$last5paramavg)), 
       ifelse(sum(lfobeta$conv_problem)>0,999,sum(lfobeta$lastparam)), 
       ifelse(sum(lfobeta$conv_problem)>0,999,sum(lfobeta$last3paramavg)), 
-      ifelse(sum(lfobeta$conv_problem)>0,999,sum(lfobeta$last5paramavg)),    
+      ifelse(sum(lfobeta$conv_problem)>0,999,sum(lfobeta$last5paramavg)), 
+      ifelse(sum(lfoalphabeta$conv_problem)>0,999,sum(lfoalphabeta$lastparam)), 
+      ifelse(sum(lfoalphabeta$conv_problem)>0,999,sum(lfoalphabeta$last3paramavg)), 
+      ifelse(sum(lfoalphabeta$conv_problem)>0,999,sum(lfoalphabeta$last5paramavg)),    
       ifelse(sum(lfohmma$conv_problem)>0,999,sum(lfohmma$lastregime_pick)),
       ifelse(sum(lfohmma$conv_problem)>0,999,sum(lfohmma$last3regime_pick)),
       ifelse(sum(lfohmma$conv_problem)>0,999,sum(lfohmma$last5regime_pick)),
@@ -107,7 +156,8 @@ for(a in seq_len(nrow(simPar))){
       )
   }
 
-lfoTMB[[a]] <-lfodf
+  lfoTMB[[a]] <- lfodf
+  lfomwTMB[[a]] <- lfomwdf
 }
 
 
