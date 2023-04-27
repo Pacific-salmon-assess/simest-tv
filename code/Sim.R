5dc065bc7b5021eeeebc47fa21bb79278d12cf57
Catarina Wor <catarinawor@gmail.com>
2023-04-27 16:57:51 -0700

    update sigmed data

--- code/Sim.R
+++ code/Sim.R
@@ -25,13 +25,14 @@ source("code/utils.R")
 #simPar <- read.csv("data/generic/SimPars.csv")
 #simPar <- read.csv("data/genericER/SimPars_ER.csv")
 #simPar <- read.csv("data/Smax_sensitivity/SimPars.csv")
-simPar <- read.csv("data/Smax_sensitivity_doublealpha/SimPars.csv")
+simPar <- read.csv("data/sensitivity_halfSmax/SimPars.csv")
+#simPar <- read.csv("data/Smax_sensitivity_doublealpha/SimPars.csv")
 
 # CU-specific parameters
 #cuPar <- read.csv("data/generic/CUPars.csv")
 #cuPar <- read.csv("data/genericER/CUPars.csv")
-cuPar <- read.csv("data/Smax_sensitivity_doublealpha/CUPars_doublealpha.csv")
-
+cuPar <- read.csv("data/sensitivity_halfSmax/CUPars_halfSmax.csv")
+#cuPar <- read.csv("data/Smax_sensitivity_doublealpha/CUPars_doublealpha.csv")
 ## Store relevant object names to help run simulation 
 scenNames <- unique(simPar$scenario)
 
